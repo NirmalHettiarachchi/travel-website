3e45e0f585b752b91455ad6b668be4cc5aab3491
NirmalHettiarachchi <nirmalhettiarachchi5@gmail.com>
2024-06-16 11:05:58 +0530

    Change assets

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CardItem from "./CardItem";
+import img2 from "../assets/images/img-2.jpg";
 import img9 from "../assets/images/img-9.jpg";
 import "./Cards.css";
 
@@ -14,6 +15,32 @@ function Cards() {
               src={img9}
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
               label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img2}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Luxury"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src={img9}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src={img2}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Luxury"
+              path="/services"
+            />
+            <CardItem
+              src={img2}
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Luxury"
               path="/services"
             />
           </ul>
